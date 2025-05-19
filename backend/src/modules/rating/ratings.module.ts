8dbbb241fae4ade4dd010ff6968bb108489c875a
Sotatek-LuanNguyen4 <126595354+Luangoc0204@users.noreply.github.com>
2025-05-19 09:48:10 +0700

    feat: add notification when rating

--- backend/src/modules/rating/ratings.module.ts
+++ backend/src/modules/rating/ratings.module.ts
@@ -1,20 +1,28 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RatingsController } from './ratings.controller';
-import { RatingsService } from './ratings.service';
+import { Notification } from 'src/typeorm/entities/Notification';
+import { CartItem } from '../../typeorm/entities/CartItem';
+import { Product } from '../../typeorm/entities/Product';
 import { Rating } from '../../typeorm/entities/Rating';
 import { User } from '../../typeorm/entities/User';
-import { Product } from '../../typeorm/entities/Product';
-import { CartItem } from '../../typeorm/entities/CartItem';
+import { NotificationService } from '../notification/notification.service';
+import { UserService } from '../users/user.service';
+import { RatingsController } from './ratings.controller';
+import { RatingsService } from './ratings.service';
+import { GatewayModule } from '../gateway/gateway.module';
+import { AccountModule } from '../account/account.module';
+import { Account } from 'src/typeorm/entities/Account';
 // Import AuthModule nếu JwtAuthGuard được export từ đó
 // import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Rating, User, Product, CartItem]),
+    TypeOrmModule.forFeature([Rating, User, Product, CartItem, Notification, Account]),
     // AuthModule, // Bỏ comment nếu bạn cần import AuthModule để sử dụng Guard
+    forwardRef(() => GatewayModule),
+    forwardRef(() => AccountModule),
   ],
   controllers: [RatingsController],
-  providers: [RatingsService],
+  providers: [RatingsService, NotificationService, UserService],
 })
 export class RatingsModule {}
